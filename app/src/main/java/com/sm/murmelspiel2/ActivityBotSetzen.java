31ae40511b3c7c2cbe42b52772559ebf80b5fb95
Tommy <tommylam979@gmail.com>
2022-01-17 13:12:27 +0100

    h

--- app/src/main/java/com/sm/murmelspiel2/ActivityBotSetzen.java
+++ app/src/main/java/com/sm/murmelspiel2/ActivityBotSetzen.java
@@ -3,12 +3,22 @@ package com.sm.murmelspiel2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class ActivityBotSetzen extends AppCompatActivity {
+
+    private TextView TVpoints;
+    private TextView TVanzahl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bot_setzen);
+        TVpoints = findViewById(R.id.TVPoints);
+        Button buttonOK = findViewById(R.id.buttonOK);
+
     }
 }
