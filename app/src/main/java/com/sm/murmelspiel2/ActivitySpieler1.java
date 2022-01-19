7f48485b956aad7615eec35324da2b6b5e4cb9ad
Sebi2682 <sebastian_menzel46@web.de>
2022-01-19 21:38:59 +0100

    vergrößern und verkleinern der gesetzten Zahl

--- app/src/main/java/com/sm/murmelspiel2/ActivitySpieler1.java
+++ app/src/main/java/com/sm/murmelspiel2/ActivitySpieler1.java
@@ -10,10 +10,13 @@ import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class ActivitySpieler1 extends AppCompatActivity implements View.OnClickListener{
+public class ActivitySpieler1 extends AppCompatActivity{
 
     private TextView tvPoints;
     private TextView textAnzahl;
+    private Button buttonOk;
+    private Button buttonKleiner;
+    private Button buttonGroesser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,9 +25,26 @@ public class ActivitySpieler1 extends AppCompatActivity implements View.OnClickL
 
         tvPoints = findViewById(R.id.tvPoints);
         textAnzahl = findViewById(R.id.textAnzahl);
-        Button buttonOk = findViewById(R.id.buttonOk);
+        buttonOk = findViewById(R.id.buttonOk);
         textAnzahl.setText(Integer.toString(MyApplication.anzahl1));
+        buttonKleiner = findViewById(R.id.buttonKleiner);
+        buttonGroesser = findViewById(R.id.buttonGroesser);
 
+        buttonKleiner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                kleiner();
+                murmelkleiner();
+            }
+        });
+
+        buttonGroesser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                groesser();
+                murmelGroesser();
+            }
+        });
 
         buttonOk.setOnClickListener(v -> openActivityRaten());
     }
@@ -38,26 +58,33 @@ public class ActivitySpieler1 extends AppCompatActivity implements View.OnClickL
 
     public void onBackPressed(){ }
 
-    @Override
-    public void onClick(View v) {
-        if (v.getId()==R.id.imageMurmelSetzen1) {
-            Animation a = AnimationUtils.loadAnimation(this,R.anim.murmel_animation);
-            a.setAnimationListener(new SimpleAnimationListener(){
-                @Override
-                public void onAnimationEnd(Animation animation) {
-                    murmelClick();
-                }
-            });
-            v.startAnimation(a);
+
+    public void kleiner(){
+        Animation a = AnimationUtils.loadAnimation(this,R.anim.murmel_animation);
+        buttonKleiner.startAnimation(a);
+    }
+
+    public void groesser(){
+        Animation a = AnimationUtils.loadAnimation(this,R.anim.murmel_animation);
+        buttonGroesser.startAnimation(a);
+    }
+
+    private void murmelkleiner() {
+
+        if(MyApplication.anzahl1 <10) {
+                MyApplication.points1--;
+                MyApplication.anzahl1++;
+            tvPoints.setText(Integer.toString(MyApplication.points1));
+            textAnzahl.setText(Integer.toString(MyApplication.anzahl1));
+
         }
     }
 
-    private void murmelClick() {
-
+    private void murmelGroesser(){
         if(MyApplication.anzahl1 >0)
-            {
-                MyApplication.points1++;
-                MyApplication.anzahl1--;
+        {
+            MyApplication.points1++;
+            MyApplication.anzahl1--;
             tvPoints.setText(Integer.toString(MyApplication.points1));
             textAnzahl.setText(Integer.toString(MyApplication.anzahl1));
 
--- app/src/main/java/com/sm/murmelspiel2/ActivitySpieler2.java
+++ app/src/main/java/com/sm/murmelspiel2/ActivitySpieler2.java
@@ -10,10 +10,13 @@ import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class ActivitySpieler2 extends AppCompatActivity implements View.OnClickListener{
+public class ActivitySpieler2 extends AppCompatActivity{
 
     private TextView tvPoints;
     private TextView textAnzahl;
+    private Button buttonOk;
+    private Button buttonKleiner2;
+    private Button buttonGroesser2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,9 +25,27 @@ public class ActivitySpieler2 extends AppCompatActivity implements View.OnClickL
 
         tvPoints = findViewById(R.id.tvPoints);
         textAnzahl = findViewById(R.id.textAnzahl);
-        Button buttonOk = findViewById(R.id.buttonOk);
+        buttonOk = findViewById(R.id.buttonOk);
         buttonOk.setOnClickListener(v -> openActivityRaten2());
         textAnzahl.setText(Integer.toString(MyApplication.anzahl2));
+        buttonKleiner2 = findViewById(R.id.buttonKleiner2);
+        buttonGroesser2 = findViewById(R.id.buttonGroesser2);
+
+        buttonKleiner2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                kleiner();
+                murmelkleiner();
+            }
+        });
+
+        buttonGroesser2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                groesser();
+                murmelGroesser();
+            }
+        });
     }
 
     public void openActivityRaten2() {
@@ -37,26 +58,35 @@ public class ActivitySpieler2 extends AppCompatActivity implements View.OnClickL
 
     public void onBackPressed(){ }
 
-    @Override
-    public void onClick(View v) {
-        if (v.getId()==R.id.imageMurmelSetzen2) {
-            Animation a = AnimationUtils.loadAnimation(this,R.anim.murmel_animation);
-            a.setAnimationListener(new SimpleAnimationListener(){
-                @Override
-                public void onAnimationEnd(Animation animation) {
-                    murmelClick();
-                }
-            });
-            v.startAnimation(a);
+    public void kleiner(){
+        Animation a = AnimationUtils.loadAnimation(this,R.anim.murmel_animation);
+        buttonKleiner2.startAnimation(a);
+    }
+
+    public void groesser(){
+        Animation a = AnimationUtils.loadAnimation(this,R.anim.murmel_animation);
+        buttonGroesser2.startAnimation(a);
+    }
+
+    private void murmelkleiner() {
+
+        if(MyApplication.anzahl1 <10) {
+            MyApplication.points1--;
+            MyApplication.anzahl1++;
+            tvPoints.setText(Integer.toString(MyApplication.points1));
+            textAnzahl.setText(Integer.toString(MyApplication.anzahl1));
+
         }
     }
 
-    private void murmelClick() {
-        if (MyApplication.anzahl2 > 0) {
-            MyApplication.points2++;
-            MyApplication.anzahl2--;
-            tvPoints.setText(Integer.toString(MyApplication.points2));
-            textAnzahl.setText(Integer.toString(MyApplication.anzahl2));
+    private void murmelGroesser(){
+        if(MyApplication.anzahl1 >0)
+        {
+            MyApplication.points1++;
+            MyApplication.anzahl1--;
+            tvPoints.setText(Integer.toString(MyApplication.points1));
+            textAnzahl.setText(Integer.toString(MyApplication.anzahl1));
+
         }
     }
 }
