6c8d9afe88636555e8c52c65394fa7b6770662bd
Sebi2682 <sebastian_menzel46@web.de>
2022-01-20 18:48:04 +0100

    Bot weiter gemacht

---
+++ app/src/main/java/com/sm/murmelspiel2/ActivityBotEnde.java
@@ -0,0 +1,14 @@
+package com.sm.murmelspiel2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityBotEnde extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bot_ende);
+    }
+}
